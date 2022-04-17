60d131bc8858bef0212fc983d99f3d70f267c15a
hau823823 <inhaulee0820@gmail.com>
2022-04-17 15:15:25 +0800

    確定能夠接收來自client端的資料庫

--- backend/member/routes/member.js
+++ backend/member/routes/member.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  console.log(req.body.test)
 });
 
 module.exports = router;
